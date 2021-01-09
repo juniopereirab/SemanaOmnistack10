e8385a21baa3d6ee4a3bb4637e6dfa84a303a73d
Damaso Junio <junio.pereirab@gmail.com>
2021-01-09 16:10:29 -0300

    Creating backend, Dev Model and store function

---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const routes = Router();
+
+const DevController = require('./controllers/DevController');
+
+routes.post('/devs', DevController.store);
+
+module.exports = routes;
